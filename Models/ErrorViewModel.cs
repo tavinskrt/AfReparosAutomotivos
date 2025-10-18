d295377b17cbb45a15d8221831f5c92e9113e50d
Otávio <otaviodacosta78@hotmail.com>
2025-10-18 19:09:23 -0300

    Template inicial do .NET MVC Core
    Criação do template inicial do projeto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace af_reparos_automotivos.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
